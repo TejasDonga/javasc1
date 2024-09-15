37bab1f3b46961bb9c89a40a37375a266b6fafba
Tejas Donga <125573847+TejasDonga@users.noreply.github.com>
2024-09-15 17:21:51 +0530

    datatype&variable

---
+++ basics/datatypes.js
@@ -0,0 +1,10 @@
+const age = 20; // number 
+const bigNumber = BigInt;// bigInt
+const name = "tejas"; // string
+const adult = true; //boolean
+let nan = null; //null
+let undefine; // undefine
+let symble = symble; // unique value 
+
+const object = object;
+
---
+++ basics/variables.js
@@ -0,0 +1,4 @@
+stast = surat;
+var name = "tejas";
+let age = 20;
+const id = 1;
