6674c4a5c69f18c5f899fa64ef6bf31c435b544c
TejasDonga <tejasdonga4@gmail.com>
2024-09-15 18:28:09 +0530

    conversion of datatypes

---
+++ basics/conversion.js
@@ -0,0 +1,21 @@
+let score = "199"
+console.log(`score: ${score}`);
+console.log(typeof score);
+
+const stringNumber = Number(score);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+// "199" => 199
+// "199rt" => NaN
+// true => 1; false => 0
+
+const user = "Tejas"
+
+const userLoggin = Boolean(user)
+console.log(userLoggin);
+console.log(typeof userLoggin);
+
+// 1 => true ; 0 => false
+//"Tejas" => true
+// "" => false;
