b7c1058b2de087074590ef8dcb36f781fdc84611
TejasDonga <tejasdonga4@gmail.com>
2024-09-16 17:44:34 +0530

    memory use datatype

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,3 +1,6 @@
+//  This is Primitive datatype
+// Primitive data type use a stack Memory 
+
 const age = 20; // number 
 const bigNumber = 283976497659824785;// bigInt
 const myName = "tejas"; // string
@@ -6,6 +9,14 @@ let nan = null; //null
 let undefine; // undefine
 let symble = "r"; // unique value 
 
+// this is Non-Primitive datatype
+// Non- Primitive datatype use a heap Memory
+
 const object = ["Hi, I am Tejas Patel"];
+const Array = [1,2,3,4,5];
+const func = function () {
+    return "HELLO"
+}
 
 console.table([age, bigNumber,myName, adult,nan,undefine,symble,object])
+
